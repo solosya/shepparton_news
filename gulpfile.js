13da8e714ac4859565c24d0c1c295609174a77e5
Lee Neenan <lneenan@pagemasters.com.au>
2017-07-25 17:21:38 +1000

    index page - half way

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,40 @@
-var gulp = require ('gulp');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var gp_rename = require("gulp-rename");
-var gutil = require('gulp-util');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var minifyCss = require("gulp-clean-css");
+var gulp        = require('gulp');
+var concat      = require('gulp-concat');
+var uglify      = require('gulp-uglify');
+var gp_rename   = require("gulp-rename");
+var gutil       = require('gulp-util');
+var sass        = require('gulp-sass');
+var sourcemaps  = require('gulp-sourcemaps');
+var minifyCss   = require("gulp-clean-css");
+var hasher      = require('gulp-hasher');
+var buster      = require('gulp-cache-buster');
 var runSequence = require('run-sequence');
 
 
 gulp.task('styles', function(callback) {
-  runSequence('sass', 'concat', 'minify-css', callback);
+    runSequence('sass', 'concat', 'minify-css', 'cache', callback);
 });
 
 
-// task
+gulp.task('cache',  function() {
+  return gulp.src('layouts/main.twig')
+    .pipe(buster({
+      tokenRegExp: /\/(concat\.min\.css)\?v=[0-9a-z]+/,
+      assetRoot: __dirname + '/static/deploy/',
+      hashes: hasher.hashes,
+    }))
+    .pipe(gulp.dest('layouts/'));
+});
+
+
 gulp.task('minify-css', function () {
     return gulp.src([
         './static/deploy/concat.css',
-    ]) // path to your file
+    ]) 
     .pipe(gp_rename({suffix: '.min'}))
     .pipe(minifyCss())
-    .pipe(gulp.dest('./static/deploy'));
+    .pipe(gulp.dest('./static/deploy'))
+    .pipe(hasher());
 });
 
 
