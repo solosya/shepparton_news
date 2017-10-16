c82f96a038887bc21849c000ebb7248173c4fb5e
Rohan Bassett <rbassett@pagemasters.com.au>
2017-10-16 14:42:40 +1100

    support dropdown menu

--- static/deploy/concat.js
+++ static/deploy/concat.js
@@ -31502,15 +31502,15 @@ $('document').ready(function() {
     });
 
 
-    // $(".sb-custom-menu > .menuContainer > ul > li").hover(function (e) {
+    $(".sb-custom-menu > .menuContainer > ul > li").hover(function (e) {
 
-    // // $(".sb-custom-menu > .menuContainer > ul > li").bind("mouseenter", function (e) {
-    //     if (pageWindow.width() > sbCustomMenuBreakPoint) {
-    //         $(this).children("ul").stop(true, false).slideToggle(0);
-    //         $(this).toggleClass('now-active');
-    //         e.preventDefault();
-    //     }
-    // });
+    // $(".sb-custom-menu > .menuContainer > ul > li").bind("mouseenter", function (e) {
+        if (pageWindow.width() > sbCustomMenuBreakPoint) {
+            $(this).children("ul").stop(true, false).slideToggle(0);
+            $(this).toggleClass('now-active');
+            e.preventDefault();
+        }
+    });
 
 
     $(".sb-custom-menu > .menuContainer > ul > li > span").on("click", function(e) {
