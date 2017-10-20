d226790b3df674754d6515067ba468b3d4a0288b
Lee Neenan <lneenan@pagemasters.com.au>
2017-10-20 16:34:50 +1100

    added js file

---
+++ static/development/js/section.js
@@ -0,0 +1,24 @@
+Acme.SectionController = function() {
+    return new Acme.section();
+}
+Acme.section = function() {
+    this.events();
+};
+
+Acme.section.prototype.events = function() 
+{
+    var totalPosts = parseInt($('main').data('article-count'));
+    var limit = parseInt($('main').data('article-limit'));
+
+    if (totalPosts >= limit) {
+        var waypoint = new Waypoint({
+            element: $('.loadMoreArticles'),
+            offset: '80%',
+            handler: function (direction) {
+                if (direction == 'down') {
+                    Acme.cardController.loadMore($(this.element), waypoint);
+                }
+            }
+        });
+    }
+}
