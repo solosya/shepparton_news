c7a61614a614c1e4d240956fb8bf3ee60825ec5f
Puck Willis <PWillis@pagemasters.com.au>
2017-11-29 14:24:03 +1100

    in-article ads I

--- static/development/js/article.js
+++ static/development/js/article.js
@@ -7,6 +7,11 @@ Acme.article = function() {
 
 Acme.article.prototype.InsertInterstitial = function() {
     if ($('.article_content > p').length >= 9) {
-        $("<div id='interAd'></div>").insertAfter( $('.article_content > p')[4] );        
+        $("<div class='teads-inread' ></div>").insertAfter( $('.article_content > p')[5] );        
+    	googletag.cmd.push(function() { googletag.display('div-gpt-ad-teads'); });       
     }
+    if ($('.article_content > p').length >= 4) {
+        $("<div class='ad-container hidden-md hidden-lg'><div id='div-gpt-ad-mrec-7' class='card__announcement-image google_ad google_ad_mrec text-center' ></div></div>").insertAfter( $('.article_content > p')[3] );        
+    	googletag.cmd.push(function() { googletag.display('div-gpt-ad-mrec-7'); });
+	}
 }
