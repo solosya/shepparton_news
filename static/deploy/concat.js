a878fda3cf7f9f004ec914c76ac4aad11b9ca2b9
Lee <lneenan@pagemasters.com.au>
2018-02-22 13:42:21 +1100

    gulp bust

--- static/deploy/concat.js
+++ static/deploy/concat.js
@@ -33978,50 +33978,62 @@ window.templates.pulldown =
 window.templates.listingSavedTmpl =  '<p>Following approval it will be posted to the events page within 24 hours.</p><div><form><button class="btn _btn dialogButton">Okay</button></form></div>';
 
                                         
-var socialCardTemplate =  '<div class="{{containerClass}}">' +
-                                '<a href="{{social.url}}"\
-                                    target="_blank"\
-                                    class="card swap social {{social.source}} {{#if social.hasMedia}} withImage__content {{else }} without__image {{/if}} {{videoClass}}"\
-                                    data-id="{{socialId}}"\
-                                    data-position="{{position}}"\
-                                    data-guid="{{social.guid}}"\
-                                    data-social="1"\
-                                    data-article-image="{{{social.media.path}}}"\
-                                    data-article-text="{{social.content}}">\
-                                    <article class="">\
-                                        {{#if social.hasMedia}}\
-                                            <figure class="{{videoClass}}">\
-                                                <img class="img-responsive" src="{{social.media.path}}" style="background-image:url(placeholder">\
-                                            </figure>\
-                                        {{/if}}\
-                                        \
-                                        <div class="content">\
-                                            <div class="category {{social.source}}">{{social.source}}</div>\
-                                            <p id="updateSocial{{article.socialId}}" data-update="0">{{{social.content}}}</p>\
-                                            <time datetime="2016-11-16"></time>\
-                                            <div class="author">\
-                                                <p class="">{{ social.user.name }}</p>\
-                                                <time datetime="{{publishDate}}">{{publishDate}}</time>\
-                                            </div>\
-                                        </div>'+
+var socialCardTemplate =  
+'<div class="{{cardClass}}"> \
+    <a  href="{{social.url}}"\
+        target="_blank"\
+        class="swap card social {{social.source}} {{#if social.hasMedia}} withImage__content {{else }} without__image {{/if}} {{videoClass}}"\
+        data-id="{{socialId}}"\
+        data-position="{{position}}"\
+        data-guid="{{social.guid}}"\
+        data-social="1"\
+        data-article-image="{{{social.media.path}}}"\
+        data-article-text="{{social.content}}">\
+        \
+        <article class="socialarticle {{social.source}} lightbox">\
+            {{#if social.hasMedia}}\
+                <figure class="{{videoClass}}">\
+                    <img class="img-responsive" src="{{social.media.path}}" style="background-image:url(placeholder">\
+                </figure>\
+            {{/if}}\
+            \
+            <div class="content">\
+                <div class="icon {{site}}"></div>\
+                \
+                <div class="category {{social.source}}">{{social.source}}</div>\
+                \
+                <p id="updateSocial{{article.socialId}}" data-update="0">{{{social.content}}}</p>\
+                \
+                <div class="author">\
+                    <img src="{{social.user.media.path}}" class="img-circle">\
+                    <p class="">{{ social.user.name }}</p>\
+                    <time datetime="{{publishDate}}">{{publishDate}}</time>\
+                </div>\
+                <time datetime="{{publishDate}}">{{publishDate}}</time>\
+            </div>'+
 
 
-                                        '{{#if userHasBlogAccess}}\
-                                            <div class="btn_overlay articleMenu">\
-                                                <button title="Hide" data-guid="{{social.guid}}" class="btnhide social-tooltip HideBlogArticle" type="button" data-social="1">\
-                                                    <i class="fa fa-eye-slash"></i><span class="hide">Hide</span>\
-                                                </button>\
-                                                <button title="Edit" class="btnhide social-tooltip editSocialPost" type="button" data-url="/admin/social-funnel/update-social?guid={{blog.guid}}&socialguid={{social.guid}}">\
-                                                <i class="fa fa-edit"></i><span class="hide">Edit</span>\
-                                                </button>\
-                                                <button data-position="{{position}}" data-social="1" data-id="{{socialId}}" title="{{pinTitle}}" class="btnhide social-tooltip PinArticleBtn" type="button" data-status="{{isPinned}}">\
-                                                    <i class="fa fa-thumb-tack"></i><span class="hide">{{pinText}}</span>\
-                                                </button>\
-                                            </div>\
-                                        {{/if}}\
-                                    </article>\
-                                </a>\
-                            </div>';
+            '{{#if userHasBlogAccess}}\
+                <div class="btn_overlay articleMenu">\
+                    <button title="Hide" data-guid="{{social.guid}}" class="btnhide social-tooltip HideBlogArticle" type="button" data-social="1">\
+                        <i class="fa fa-eye-slash"></i>\
+                        <span class="hide">Hide</span>\
+                    </button>\
+                    \
+                    <button title="Edit" class="btnhide social-tooltip editSocialPost" type="button" data-url="/admin/social-funnel/update-social?guid={{blog.guid}}&socialguid={{social.guid}}">\
+                    <i class="fa fa-edit"></i>\
+                    <span class="hide">Edit</span>\
+                    </button>\
+                    \
+                    <button data-position="{{position}}" data-social="1" data-id="{{socialId}}" title="{{pinTitle}}" class="btnhide social-tooltip PinArticleBtn" type="button" data-status="{{isPinned}}">\
+                        <i class="fa fa-thumb-tack"></i>\
+                        <span class="hide">{{pinText}}</span>\
+                    </button>\
+                </div>\
+            {{/if}}\
+        </article>\
+    </a>\
+</div>';
 
 
 var socialPostPopupTemplate = function(channel) {
