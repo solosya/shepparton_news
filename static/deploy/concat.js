6377d5c8057e5e517bc982cc4f3c823fb4804cfd
Puck Willis <PWillis@pagemasters.com.au>
2018-03-22 13:53:44 +1100

    event pages and in-article ad

--- static/deploy/concat.js
+++ static/deploy/concat.js
@@ -34259,8 +34259,8 @@ Acme.article.prototype.InsertInterstitial = function() {
         // $("<div class='teads-inread' ></div>").insertAfter( $('.article_content > p')[5] );        
     	googletag.cmd.push(function() { googletag.display('div-gpt-ad-teads'); });       
     }
-    if ($('.article_content > p').length >= 4) {
-        $("<div class='ad-container hidden-md hidden-lg mobad2' style='position:relative;width:300px;margin: 0 auto;'><div id='div-gpt-ad-mrec-2' class='card__announcement-image google_ad google_ad_mrec text-center' ></div></div>").insertAfter( $('.article_content > p')[2] );        
+    if ($('.article_content > p').length >= 7) {
+        $("<div class='ad-container hidden-md hidden-lg mobad2' style='position:relative;width:300px;margin: 0 auto;'><div id='div-gpt-ad-mrec-2' class='card__announcement-image google_ad google_ad_mrec text-center' ></div></div>").insertAfter( $('.article_content > p')[6] );        
     	googletag.cmd.push(function() { googletag.display('div-gpt-ad-mrec-2'); });
 	}
 }
