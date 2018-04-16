a566d00af69c92054a0b41ca768846ea4a7b7bf2
Puck Willis <PWillis@pagemasters.com.au>
2018-04-16 13:56:31 +1000

    moved ads.js to articleFeeds

--- static/deploy/concat.js
+++ static/deploy/concat.js
@@ -34202,6 +34202,251 @@ var socialPostPopupTemplate = function(channel) {
      '</div>'   ;   
 }
 
+Acme.ArticleController = function() {
+    return new Acme.article();
+}
+Acme.article = function() {
+    // this.events();
+    //this.showOovvuu();
+};
+
+Acme.article.prototype.insertOovvuu = function() {
+    articleContent = $("#articleContent").children();
+    oovvuu = jQuery.parseJSON($("#oovvuu").text());
+    brightcove = $("#brightcove").text().trim();
+    if (brightcove == '') {brightcove = "5370537724001";} 
+    if (oovvuu.status_code == 200) {
+        oovvuu = oovvuu.embedCodes.group1;
+        minPars = 9;
+        targetPars = [3, 100];
+        for (i=0; i < targetPars.length; i++) {
+
+            //if ($('.article_content > p').length > targetPars[i]) {
+
+                if (oovvuu.length > i) {
+
+                    content = 
+                    '<div class="oovvuu-video"> \
+                        <div style="padding-top: 56.25%;">\
+                            <iframe src="//players.brightcove.net/'+brightcove+'/default_default/index.html?videoId='+oovvuu[i].embed_code+'" \
+                                    allowfullscreen\
+                                    webkitallowfullscreen\
+                                    mozallowfullscreen\
+                                    style="width: 100%; height: 100%; position: absolute; top: 0px; bottom: 0px; right: 0px; left: 0px;"> \
+                            </iframe>\
+                        </div> \
+                    </div> \
+                    <p class="oovvuu-video__text">Related videos powered by Oovvuu</p>';
+
+                    targetPar = targetPars[i];
+                    if( $('.article_content > p').length < targetPars[i]) {
+                        targetPar =  $('.article_content > p').length-1;
+                        i = targetPars.length;
+                        if ($('.article_content > p').length < minPars) {
+                            targetPar = -1;
+                        }
+                    }
+                    if (targetPar > 0) {
+                        $(content).insertAfter( $('.article_content > p')[targetPar-1]);
+                    }
+                }
+            //}
+        }
+    }
+        
+
+    
+};
+
+
+Acme.article.prototype.InsertInterstitial = function() {
+    if ($('.article_content > p').length >= 9) {
+        // $("<div class='teads-inread' ></div>").insertAfter( $('.article_content > p')[5] );        
+    	teadsAd = true;       
+    }
+    if ($('.article_content > p').length >= 7) {
+        $("<div class='ad-container hidden-md hidden-lg mobad2' style='position:relative;width:300px;margin: 0 auto;'><div id='article-mrec-2-inarticle-mobile' class='advert' ></div></div>").insertAfter( $('.article_content > p')[6] );
+	}
+}
+
+Acme.GalleryToggle = function() {
+    console.log('running gallery events');
+    this.events();
+};
+Acme.GalleryToggle.prototype.events = function() {
+    $('#gallery-toggle').on('click', function(e) {
+        var option = $(e.target);
+
+        if (option.hasClass('gallery-toggle__item')) {
+            console.log(option);
+            var types = ['image', 'video'];
+            // var component = $(this);
+            // var list = component.children('ul');
+            var items = $('.gallery-toggle__item').removeClass('gallery-toggle__item--selected');
+            option.addClass('gallery-toggle__item--selected');
+            var type = types.indexOf( option.data('type') );
+
+            // var z_index = type ? '3' : '1';
+            // $( $('.owl-carousel')[1] ).css('z-index', z_index);
+            // console.log($( $('.owl-carousel')[0])) ;
+            var show = $('.owl-gallery-' + types[type]);
+            var hide = $('.owl-gallery-' + types[!type | 0]);
+            hide.removeClass('default');
+            show.addClass('default');
+
+        }
+
+
+        console.log(option);
+    });
+};
+Acme.View.articleFeed = function(cardModel, limit, offset, infinite, failText, loadmax) {
+    this.cardModel = cardModel;
+    this.offset    = offset || 0;
+    this.limit     = limit || 10;
+    this.infinite  = infinite || false;
+    this.waypoint  = false;
+    this.options   = {};
+    this.loadcount = 0;
+    this.loadmax   = loadmax || null;
+    this.elem      = $('.loadMoreArticles');
+    this.failText  = failText || null;
+    this.events();
+};
+
+Acme.View.articleFeed.prototype.fetch = function()
+{
+    console.log('fetching');
+    var self = this;
+    self.elem.html("Please wait...");
+
+    var container = $('#'+self.elem.data('container'));
+
+    // blogfeed makes 2 sql calls.  
+    //      Offset is to get pinned contect 
+    //      nonPinnedOffset gets the rest
+    //      They're combined to return full result
+    self.options = {
+        'container'         :   container,
+        'limit'             :   self.elem.data('limit'),
+        'offset'            :   self.elem.data('offset') || self.elem.data('limit'),
+        'nonPinnedOffset'   :   self.elem.data('non-pinned-offset') || -1,
+        'blogid'            :   self.elem.data('blogguid'),
+        'loadtype'          :   self.elem.data('loadtype')      || "home",
+        'search'            :   self.elem.data('searchterm')    || null,
+        'after'             :   self.elem.data('after')  || null,
+        'before'            :   self.elem.data('before')  || null,
+    };
+
+    if (self.options.search != null) {
+        self.options.blogid = self.elem.data("blogid"); // search takes an id instead of a guid
+    }
+
+    $.fn.Ajax_LoadBlogArticles(self.options).done(function(data) {
+
+        if (data.success == 1) {
+            self.loadcount++;
+
+            if ( self.loadmax !== null && self.loadcount >= self.loadmax ) {
+                self.waypoint.destroy();
+            }
+
+            self.render(data);
+        }
+    });
+};
+
+
+Acme.View.articleFeed.prototype.render = function(data) 
+{
+    var self = this;
+
+    var cardClass  =   self.elem.data('card-class'),
+        template   =   self.elem.data('card-template') || null,
+        label      =   self.elem.data('button-label')  || "Load more",
+        ads_on     =   self.elem.data('ads')           || null,
+        rendertype =   self.elem.data('rendertype')    || null;
+
+    self.elem.html(label);
+
+    (data.articles.length < self.options.limit) 
+        ? self.elem.css('display', 'none')
+        : self.elem.show();
+
+    // add counts to the dom for next request
+    self.elem.data('non-pinned-offset', data.existingNonPinnedCount);
+    self.elem.data('offset', (self.options.offset + self.options.limit));
+
+    var html = [];
+    if (ads_on == "yes") {
+        html.push( window.templates.ads_infinite );
+    }
+
+
+    if (data.articles.length === 0 && self.failText) {
+        html = ["<p>" + self.failText + "</p>"];
+    } else {
+        for (var i in data.articles) {
+            if (self.options.before) {
+                html.push( self.options.before );
+            }
+
+            html.push( self.cardModel.renderCard(data.articles[i], cardClass, template) );
+
+            if (self.options.after) {
+                html.push( self.options.after );
+            }
+        }
+    }
+    (rendertype === "write")
+        ? self.options.container.empty().append( html.join('') )
+        : self.options.container.append( html.join('') );
+        
+    if (self.waypoint) {
+        (data.articles.length < self.options.limit)
+            ? self.waypoint.disable()
+            : self.waypoint.enable();
+    }
+
+    $(".card .content > p, .card h2").dotdotdot();     
+    // $('.video-player').videoPlayer();
+    $("div.lazyload").lazyload({
+        effect: "fadeIn"
+    });
+
+    this.cardModel.events_refresh();
+
+
+    self.elem.data('rendertype', '');
+
+    if (ads_on == "yes") {
+       self.InsertAds();
+    }
+};
+
+
+Acme.View.articleFeed.prototype.events = function() 
+{
+    var self = this;
+    self.elem.unbind().on('click', function(e) {
+        e.preventDefault();
+        self.fetch();
+    });
+
+    if (this.infinite && this.offset >= this.limit) {
+        console.log('setting infinite');
+        self.waypoint = new Waypoint({
+            element: self.elem,
+            offset: '100%',
+            handler: function (direction) {
+                if (direction == 'down') {
+                    self.fetch();
+                }
+            }
+        });
+    }
+};
+
 Acme.InsertAds = function() {
     var pageAdSlots = $('.advert');
     //console.log('pageAdSlots:');
@@ -34345,253 +34590,6 @@ Acme.InsertAds = function() {
     function generateNextAdName(basename) {
         var id = nextAdId++;
         return basename + id;
-    }
-};
-
-
-Acme.ArticleController = function() {
-    return new Acme.article();
-}
-Acme.article = function() {
-    // this.events();
-    //this.showOovvuu();
-};
-
-Acme.article.prototype.insertOovvuu = function() {
-    articleContent = $("#articleContent").children();
-    oovvuu = jQuery.parseJSON($("#oovvuu").text());
-    brightcove = $("#brightcove").text().trim();
-    if (brightcove == '') {brightcove = "5370537724001";} 
-    if (oovvuu.status_code == 200) {
-        oovvuu = oovvuu.embedCodes.group1;
-        minPars = 9;
-        targetPars = [3, 100];
-        for (i=0; i < targetPars.length; i++) {
-
-            //if ($('.article_content > p').length > targetPars[i]) {
-
-                if (oovvuu.length > i) {
-
-                    content = 
-                    '<div class="oovvuu-video"> \
-                        <div style="padding-top: 56.25%;">\
-                            <iframe src="//players.brightcove.net/'+brightcove+'/default_default/index.html?videoId='+oovvuu[i].embed_code+'" \
-                                    allowfullscreen\
-                                    webkitallowfullscreen\
-                                    mozallowfullscreen\
-                                    style="width: 100%; height: 100%; position: absolute; top: 0px; bottom: 0px; right: 0px; left: 0px;"> \
-                            </iframe>\
-                        </div> \
-                    </div> \
-                    <p class="oovvuu-video__text">Related videos powered by Oovvuu</p>';
-
-                    targetPar = targetPars[i];
-                    if( $('.article_content > p').length < targetPars[i]) {
-                        targetPar =  $('.article_content > p').length-1;
-                        i = targetPars.length;
-                        if ($('.article_content > p').length < minPars) {
-                            targetPar = -1;
-                        }
-                    }
-                    if (targetPar > 0) {
-                        $(content).insertAfter( $('.article_content > p')[targetPar-1]);
-                    }
-                }
-            //}
-        }
-    }
-        
-
-    
-};
-
-
-Acme.article.prototype.InsertInterstitial = function() {
-    if ($('.article_content > p').length >= 9) {
-        // $("<div class='teads-inread' ></div>").insertAfter( $('.article_content > p')[5] );        
-    	teadsAd = true;       
-    }
-    if ($('.article_content > p').length >= 7) {
-        $("<div class='ad-container hidden-md hidden-lg mobad2' style='position:relative;width:300px;margin: 0 auto;'><div id='article-mrec-2-inarticle-mobile' class='advert' ></div></div>").insertAfter( $('.article_content > p')[6] );
-	}
-}
-
-Acme.GalleryToggle = function() {
-    console.log('running gallery events');
-    this.events();
-};
-Acme.GalleryToggle.prototype.events = function() {
-    $('#gallery-toggle').on('click', function(e) {
-        var option = $(e.target);
-
-        if (option.hasClass('gallery-toggle__item')) {
-            console.log(option);
-            var types = ['image', 'video'];
-            // var component = $(this);
-            // var list = component.children('ul');
-            var items = $('.gallery-toggle__item').removeClass('gallery-toggle__item--selected');
-            option.addClass('gallery-toggle__item--selected');
-            var type = types.indexOf( option.data('type') );
-
-            // var z_index = type ? '3' : '1';
-            // $( $('.owl-carousel')[1] ).css('z-index', z_index);
-            // console.log($( $('.owl-carousel')[0])) ;
-            var show = $('.owl-gallery-' + types[type]);
-            var hide = $('.owl-gallery-' + types[!type | 0]);
-            hide.removeClass('default');
-            show.addClass('default');
-
-        }
-
-
-        console.log(option);
-    });
-};
-Acme.View.articleFeed = function(cardModel, limit, offset, infinite, failText, loadmax, adsModel) {
-    this.cardModel = cardModel;
-    this.adsModel  = adsModel;
-    this.offset    = offset || 0;
-    this.limit     = limit || 10;
-    this.infinite  = infinite || false;
-    this.waypoint  = false;
-    this.options   = {};
-    this.loadcount = 0;
-    this.loadmax   = loadmax || null;
-    this.elem      = $('.loadMoreArticles');
-    this.failText  = failText || null;
-    this.events();
-};
-
-Acme.View.articleFeed.prototype.fetch = function()
-{
-    console.log('fetching');
-    var self = this;
-    self.elem.html("Please wait...");
-
-    var container = $('#'+self.elem.data('container'));
-
-    // blogfeed makes 2 sql calls.  
-    //      Offset is to get pinned contect 
-    //      nonPinnedOffset gets the rest
-    //      They're combined to return full result
-    self.options = {
-        'container'         :   container,
-        'limit'             :   self.elem.data('limit'),
-        'offset'            :   self.elem.data('offset') || self.elem.data('limit'),
-        'nonPinnedOffset'   :   self.elem.data('non-pinned-offset') || -1,
-        'blogid'            :   self.elem.data('blogguid'),
-        'loadtype'          :   self.elem.data('loadtype')      || "home",
-        'search'            :   self.elem.data('searchterm')    || null,
-        'after'             :   self.elem.data('after')  || null,
-        'before'            :   self.elem.data('before')  || null,
-    };
-
-    if (self.options.search != null) {
-        self.options.blogid = self.elem.data("blogid"); // search takes an id instead of a guid
-    }
-
-    $.fn.Ajax_LoadBlogArticles(self.options).done(function(data) {
-
-        if (data.success == 1) {
-            self.loadcount++;
-
-            if ( self.loadmax !== null && self.loadcount >= self.loadmax ) {
-                self.waypoint.destroy();
-            }
-
-            self.render(data);
-        }
-    });
-};
-
-
-Acme.View.articleFeed.prototype.render = function(data) 
-{
-    var self = this;
-
-    var cardClass  =   self.elem.data('card-class'),
-        template   =   self.elem.data('card-template') || null,
-        label      =   self.elem.data('button-label')  || "Load more",
-        ads_on     =   self.elem.data('ads')           || null,
-        rendertype =   self.elem.data('rendertype')    || null;
-
-    self.elem.html(label);
-
-    (data.articles.length < self.options.limit) 
-        ? self.elem.css('display', 'none')
-        : self.elem.show();
-
-    // add counts to the dom for next request
-    self.elem.data('non-pinned-offset', data.existingNonPinnedCount);
-    self.elem.data('offset', (self.options.offset + self.options.limit));
-
-    var html = [];
-    if (ads_on == "yes") {
-        html.push( window.templates.ads_infinite );
-    }
-
-
-    if (data.articles.length === 0 && self.failText) {
-        html = ["<p>" + self.failText + "</p>"];
-    } else {
-        for (var i in data.articles) {
-            if (self.options.before) {
-                html.push( self.options.before );
-            }
-
-            html.push( self.cardModel.renderCard(data.articles[i], cardClass, template) );
-
-            if (self.options.after) {
-                html.push( self.options.after );
-            }
-        }
-    }
-    (rendertype === "write")
-        ? self.options.container.empty().append( html.join('') )
-        : self.options.container.append( html.join('') );
-        
-    if (self.waypoint) {
-        (data.articles.length < self.options.limit)
-            ? self.waypoint.disable()
-            : self.waypoint.enable();
-    }
-
-    $(".card .content > p, .card h2").dotdotdot();     
-    // $('.video-player').videoPlayer();
-    $("div.lazyload").lazyload({
-        effect: "fadeIn"
-    });
-
-    this.cardModel.events_refresh();
-
-
-    self.elem.data('rendertype', '');
-
-    if (ads_on == "yes") {
-       self.adsModel();
-    }
-};
-
-
-Acme.View.articleFeed.prototype.events = function() 
-{
-    var self = this;
-    self.elem.unbind().on('click', function(e) {
-        e.preventDefault();
-        self.fetch();
-    });
-
-    if (this.infinite && this.offset >= this.limit) {
-        console.log('setting infinite');
-        self.waypoint = new Waypoint({
-            element: self.elem,
-            offset: '100%',
-            handler: function (direction) {
-                if (direction == 'down') {
-                    self.fetch();
-                }
-            }
-        });
     }
 };
 
