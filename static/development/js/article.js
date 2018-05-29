f7aeb1ab1434f23891b918eefe7211d4a2bfc8e0
Lee <lneenan@pagemasters.com.au>
2018-05-29 13:56:55 +1000

    gallery image sizing updates specific to gallery layout, not galleries within normal articles

--- static/development/js/article.js
+++ static/development/js/article.js
@@ -5,68 +5,6 @@ Acme.article = function() {
     this.events();
 };
 
-// Acme.article.prototype.getOovvuuVideos = function() {
-//     console.log('getting oovvuu videos');
-//     var url = 'https://905wo67qr6.execute-api.us-west-2.amazonaws.com/v1/MMGP/list/publisher/47?limit=3';
-
-//     Acme.server.fetch(url).then(function(r) {
-//         console.log(r);
-//     });
-// }
-
-
-
-// Acme.article.prototype.insertOovvuu = function() {
-//     articleContent = $("#articleContent").children();
-//     try {
-//         oovvuu = jQuery.parseJSON($("#oovvuu").text());
-//         brightcove = $("#brightcove").text().trim();
-//         if (brightcove == '') {brightcove = "5370537724001";} 
-//         if (oovvuu.status_code == 200) {
-//             oovvuu = oovvuu.embedCodes.group1;
-//             minPars = 9;
-//             targetPars = [3, 100];
-//             for (i=0; i < targetPars.length; i++) {
-
-//                 //if ($('.article_content > p').length > targetPars[i]) {
-
-//                     if (oovvuu.length > i) {
-
-//                         content = 
-//                         '<div class="oovvuu-video"> \
-//                             <div style="padding-top: 56.25%;">\
-//                                 <iframe src="//players.brightcove.net/'+brightcove+'/default_default/index.html?videoId='+oovvuu[i].embed_code+'" \
-//                                         allowfullscreen\
-//                                         webkitallowfullscreen\
-//                                         mozallowfullscreen\
-//                                         style="width: 100%; height: 100%; position: absolute; top: 0px; bottom: 0px; right: 0px; left: 0px;"> \
-//                                 </iframe>\
-//                             </div> \
-//                         </div> \
-//                         <p class="oovvuu-video__text">Related videos powered by Oovvuu</p>';
-
-//                         targetPar = targetPars[i];
-//                         if( $('.article_content > p').length < targetPars[i]) {
-//                             targetPar =  $('.article_content > p').length-1;
-//                             i = targetPars.length;
-//                             if ($('.article_content > p').length < minPars) {
-//                                 targetPar = -1;
-//                             }
-//                         }
-//                         if (targetPar > 0) {
-//                             $(content).insertAfter( $('.article_content > p')[targetPar-1]);
-//                         }
-//                     }
-//                 //}
-//             }
-//         }
-//     } catch(err) {
-//         console.log(err);
-//     }
-        
-
-    
-// };
 
 
 Acme.article.prototype.InsertInterstitial = function() {
@@ -118,6 +56,8 @@ Acme.GalleryToggle.prototype.events = function() {
             var hide = $('.owl-gallery-' + types[!type | 0]);
             hide.removeClass('default');
             show.addClass('default');
+
+            $('.article-video').muted = true;
 
         }
     });
