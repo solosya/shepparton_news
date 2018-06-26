7c339f24b4e8252fdad958e91ce1a9e08a905860
Puck Willis <PWillis@pagemasters.com.au>
2018-06-26 17:06:32 +1000

    made sure finley exists

--- static/deploy/concat.js
+++ static/deploy/concat.js
@@ -37628,7 +37628,7 @@ UserArticlesController.Load = (function ($) {
         } else if (siteLocation.search('seymour') >= 0 ) {
             location = '​Australia/Seymour';
         } else if (siteLocation.search('southern') >= 0 ) {
-            location = 'Australia/Finley​';
+            location = 'Australia/Finley';
         } else if (siteLocation.search('tatura') >= 0 ) {
             location = 'Australia/Tatura';
         } else if (siteLocation.search('denipt') >= 0 || siteLocation.search('deniliquin') >= 0) {
