5e154860963ca3281b6cabe50eaf0debb395bac2
Lee <lneenan@pagemasters.com.au>
2019-06-18 15:07:27 +1000

    fix view all links

---
+++ theme_config.js
@@ -0,0 +1,83 @@
+[{
+  "active": true,
+  "weather": {
+    "location": "Australia/Shepparton"
+  },
+  "page": {
+    "index": {
+      "active": true,
+      "sections": [
+        {
+          "active": true,
+          "template": "partials/index/section-15.twig",
+          "articleOrder": [
+            "aapp",
+            "aapp",
+            "aapp",
+            "aapp",
+            "aapp",
+            "australia",
+            "australia",
+            "australia"
+          ],
+          "adslots": [
+            "mrec-1"
+          ]
+        },
+        {
+          "title": "News",
+          "blog": "australia",
+          "articleCount": 4,
+          "template": "partials/index/section-01",
+          "template_params": {
+            "loadmore": true
+          }
+        },
+        {
+          "title": "Local sport",
+          "template": "partials/index/section-02",
+          "template_params": {
+            "loadmore": true
+          },
+          "blog": "business",
+          "articleCount": 4,
+          "adslots": [
+            "mrec-2"
+          ]
+        },
+        {
+          "title": "Sunraysia Life",
+          "template": "partials/index/section-03",
+          "template_params": {
+            "loadmore": true
+          },
+          "background": true,
+          "blog": "australia",
+          "articleCount": 4
+        },
+        {
+          "title": "Classifieds",
+          "template": "partials/index/section-04",
+          "template_params": {
+            "loadmore": true
+          },
+          "blog": "australia",
+          "articleCount": 4
+        },
+        {
+          "title": "Opinion",
+          "template": "partials/index/section-04",
+          "blog": "australia",
+          "articleCount": 4
+        },
+        {
+          "title": "Feature Publications",
+          "template": "partials/index/section-03",
+          "background": true,
+          "blog": "australia",
+          "articleCount": 4
+        }
+      ]
+    }
+  }
+}]
