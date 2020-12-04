bd3563e1f9dc024ed9ed4ebed6bafe5b693386b5
Lee <Leeneenan@gmail.com>
2020-12-04 17:14:10 +1100

    update infinite ads template

--- static/deploy/concat.js
+++ static/deploy/concat.js
@@ -21660,9 +21660,9 @@ Acme.property_card =
 
 
 window.templates.ads_infinite = 
-    "<div class='advert-desktop advert-tablet col-sm-9 hidden-xs u-margin-top-30' data-adsize='banner' style='padding:0;'></div>\
-    <div class='col-xs-9 visible-xs-block' style='padding:0;width:300px;height:250px;'><div class='advert-mobile' data-adsize='mrec'></div></div>\
-    <hr class='divide18 col-xs-9 visible-xs-block'>";
+    "<div class='advert-desktop advert-tablet col-12 d-none d-md-block u-margin-top-30' data-adsize='banner' style='padding:0;'></div>\
+    <div class='col-12 d-block d-md-none' style='padding:0;width:300px;height:250px;'><div class='advert-mobile' data-adsize='mrec'></div></div>\
+    <hr class='divide18 col-xs-9 d-block d-md-none'>";
 
 
 
